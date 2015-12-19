94c7e06fd2f28544ecb8b278f60fbcd8fb85e0e1
Winston Wang <winstonwang12@gmail.com>
2015-12-18 23:24:40 -0800

    added correct routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
-var router = express.Router();
+var router = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Unit Coverter' });
+});
+
+router.get(/ap*/, function(req, res) {
+  res.json('request: ' + req.url);
 });
 
 module.exports = router;
